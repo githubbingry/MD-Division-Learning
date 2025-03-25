42ed1190f4429ad0eb69fea1120b591282fa0f15
githubbingry <fachry302405@gmail.com>
2025-03-26 01:14:49 +0700

    fix: making debug flag disappear and for now deleting theme on materialapp

--- fachryghifary/app/flutter_app/lib/main.dart
+++ fachryghifary/app/flutter_app/lib/main.dart
@@ -11,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Profile Page',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SimpleProfilePage(),
       ),
